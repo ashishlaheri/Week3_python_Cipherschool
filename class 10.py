733fe856ba8c242dc0fffa776c2a08f3d82707da
Ashish kumar laheri <118184953+ashishlaheri@users.noreply.github.com>
2022-12-26 21:17:59 +0530

    3rd week codes

---
+++ class 10.py
@@ -0,0 +1,7 @@
+def rev(s):
+    lis2=[i[::-1] for i in s]
+    return lis2
+
+
+list1=["","ashish","chero bhaia","hero"]
+print(rev(list1))
---
+++ intro_to_kwargs.py
@@ -0,0 +1,5 @@
+def func(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+# ** will take argument as a dictionary
+func(name="Ashish kumar laheri",roll_no="16")
